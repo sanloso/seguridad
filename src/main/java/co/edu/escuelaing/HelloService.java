d954535f51dc08cce6488ca5d2c499a77f60de3b
sanloso <sanloso25@hotmail.com>
2021-10-10 21:59:56 -0500

    first commit

---
+++ src/main/java/co/edu/escuelaing/HelloService.java
@@ -0,0 +1,21 @@
+package co.edu.escuelaing;
+
+import static spark.Spark.*;
+
+public class HelloService
+{
+    public static void main( String[] args )
+    {
+        //API: secure(keystoreFilePath, keystorePassword, truststoreFilePath,truststorePassword);
+        secure("keystores/ecikeystore.p12", "123456", null, null);
+        port(getPort());
+        get("/hello", (req, res) -> "Hello World");
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+}
